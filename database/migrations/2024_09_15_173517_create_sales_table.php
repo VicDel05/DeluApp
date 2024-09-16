f4f8b2661cb3a736348ff623dfac2c9de92ff4b3
VicDel05 <badelvuctor@gmail.com>
2024-09-15 23:43:14 -0600

    Configuracion de proyecto y migraciones

---
+++ database/migrations/2024_09_15_173517_create_sales_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id')->constrained('users')->onDelete('cascade'); // Relaciona con la tabla de clientes
+            $table->date('fecha_venta'); // Fecha de la venta
+            $table->decimal('total', 8, 2); // Total de la venta
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
